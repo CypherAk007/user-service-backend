39ea575671d2e414bea801e9e4868a11f8f1f952
ABHISHEK KRISHNA <ktmabhi007@gmail.com>
2025-08-08 17:52:51 +0530

    JWT: Take the token from request bodey

--- src/main/java/com/backend/ecommerceuserservice/controllers/UserController.java
+++ src/main/java/com/backend/ecommerceuserservice/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.backend.ecommerceuserservice.dtos.*;
 import com.backend.ecommerceuserservice.models.Token;
 import com.backend.ecommerceuserservice.models.User;
 import com.backend.ecommerceuserservice.service.UserService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,8 +42,28 @@ public class UserController {
         userService.logout(logoutRequestDTO.getTokenValue());
         return ResponseEntity.ok("Logout successful.");
     }
-    @GetMapping("/validate/{token}")
-    public ResponseEntity<Boolean> validateToken(@PathVariable("token") String token){
+//    @GetMapping("/validate/{token}")
+//    public ResponseEntity<Boolean> validateToken(@PathVariable("token") String token){
+//        User user = userService.validateToken(token);
+//        ResponseEntity<Boolean> responseEntity;
+//
+//        if(user==null){
+//            responseEntity = new ResponseEntity<>(false, HttpStatus.UNAUTHORIZED);
+//        }else{
+//            responseEntity = new ResponseEntity<>(true,HttpStatus.OK);
+//        }
+//        return responseEntity;
+//
+//    }
+
+////    CHANGING THE TOKEN GIVEN AT URL PATH TO REQUEST HEADERS
+//// @RequestHeader(HttpHeaders.AUTHORIZATION) - > name the header Authorization and add Bearer- Best Practice
+    @PostMapping("/validate")
+    public ResponseEntity<Boolean> validateToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String token){
+        if(token.startsWith("Bearer ")){
+//            token  = token.substring(7);
+            token = token.replace("Bearer ","");
+        }
         User user = userService.validateToken(token);
         ResponseEntity<Boolean> responseEntity;
 
@@ -54,6 +75,7 @@ public class UserController {
         return responseEntity;
 
     }
+
 
 
 }
